69d680c0d352b2ec26e5e88b1b5543d5be1e7d2a
adisidev <64905594+adisidev@users.noreply.github.com>
2025-07-16 00:55:08 +0800

    Moved old results to archive

--- compare_best_results_out_of_10.r
+++ compare_best_results_out_of_10.r
@@ -6,8 +6,8 @@ library(janitor)
 
 # Set `_F4Carto`, `5FCarto`, and `flow_based` directories
 dir_F4Carto <- "./F4Carto-raw-10"
-dir_5FCarto <- "./5FCarto-raw-graded"
-dir_flow_based <- "./flow-based-raw-graded"
+dir_5FCarto <- "./5FCarto_QCLF-9"
+dir_flow_based <- "./BFB_QCLF-9"
 dir_inputs <- "./inputs"
 dir_output <- "./results_graded"
 
